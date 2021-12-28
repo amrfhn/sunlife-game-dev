8f7b7439a7be4a1b73aa56cad09be7281635cb46
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2021-12-28 22:53:07 +0800

    step how to play - carousel modal

--- src/js/components/carousels.js
+++ src/js/components/carousels.js
@@ -1,97 +1,30 @@
-import Swiper from 'swiper';
+import Swiper from "swiper";
 
-const variant1 = new Swiper('.carousel.variant-hero', {
-    pagination: {
-        el: '.carousel-controls .swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.carousel-controls .swiper-button-next',
-        prevEl: '.carousel-controls .swiper-button-prev'
-    },
-    loop: true,
-    autoplay: {
-        delay: 5000
-    }
-})
-
-const variant2 = new Swiper('.carousel.variant-products', {
-    slidesPerView: 'auto',
-    spaceBetween: 60,
-    slidesOffsetBefore: 16,
-    pagination: {
-        el: '.carousel-controls .swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.carousel-controls .swiper-button-next',
-        prevEl: '.carousel-controls .swiper-button-prev'
-    },
-    breakpoints: {
-        768: {
-            slidesOffsetBefore: 48
-        }
-    }
-})
-
-const variant3 = new Swiper('.carousel.variant-timeline', {
-    slidesPerView: 1,
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true
-    },
-    breakpoints: {
-        768: {
-            slidesPerView: 3
-        }
-    }
-})
-
-const variant4 = new Swiper('.carousel.variant-media', {
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    loop: true,
-    autoplay: {
-        delay: 5000
-    }
-})
-
-const variant5 = new Swiper('.carousel.variant-group-structure', {
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    loop: true,
-    loopedSlides: 3,
-    slidesPerView: 'auto',
-    autoplay: {
-        delay: 50000
-    },
-    breakpoints: {
-        480: {
-            slidesOffsetBefore: 48
-        }
-    }
-})
-
-const variantClientLogos = new Swiper('.carousel.variant-client-logos', {
-    slidesPerView: 3,
-    slidesPerColumn: 3,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    autoplay: {
-        delay: 3000
-    }
-})
+var fraction = document.getElementById("fraction");
+var slides = document.querySelectorAll(".swiper-slide");
+var slideCount = slides.length;
+fraction.textContent = `1 / ${slideCount}`;
+$(function () {
+  $(".modal").on("show.bs.modal", function () {
+    setTimeout(function () {
+      const steosCarousel = new Swiper(".carousel.steps-to-play", {
+        slidesPerView: 1,
+        pagination: {
+          el: ".carousel-controls .swiper-pagination",
+          // type: "fraction",
+          // clickable: true,
+        },
+        navigation: {
+          nextEl: ".carousel-controls .swiper-button-next",
+          prevEl: ".carousel-controls .swiper-button-prev",
+        },
+        on: {
+          slideChange: () => {
+            fraction.textContent = `${steosCarousel.realIndex + 1}/${slideCount}`;
+          },
+        },
+        loop: false,
+      });
+    }, 500);
+  });
+});
