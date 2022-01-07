679c3e8bb17144cc02b595e93f939b7c1bd42149
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-01-07 23:33:37 +0800

    working on login

--- src/js/components/forms.js
+++ src/js/components/forms.js
@@ -1,18 +1,95 @@
-// import Vue from 'vue'
-// import { ValidationProvider, ValidationObserver } from 'vee-validate';
+import Vue from "vue";
+import { ValidationProvider, ValidationObserver } from "vee-validate";
 // import _ from 'lodash';
 // import VueRecaptcha from 'vue-recaptcha';
 
 $(function () {
-    if ($('#registerPlayer').length){
-        console.log('register form page')
+  if ($("#register-form").length) {
+    const registerForm = new Vue({
+      el: "#register-form",
+      data: {
+        formData: {
+          Fullname: "",
+          Email: "",
+          IcNumber: "",
+          ContactNumber: "",
+          IsCheckedTNC: "", //checked
+        },
+        generalSubmitError: "",
+      },
+      components: {
+        ValidationProvider,
+        ValidationObserver,
+      },
+      mounted: function () {
+        console.log("register form page");
+
+        //for local test
+        var userData = new Array();
+        var loginInfo = new Object();
+
+        loginInfo = {
+          id: "123456787654",
+          name: "Amir Farhan",
+          email: "amir@gmail.com",
+          status: "authorized from facebook",
+        };
+
+        userData.push(loginInfo);
+        sessionStorage.setItem("fbUserData", JSON.stringify(userData));
+        /*************/
+
         var userData = JSON.parse(sessionStorage.getItem("fbUserData"));
-        console.log(userData)
-    }
-})
+        console.log(userData);
+
+        this.formData.Fullname = userData[0].name;
+        this.formData.Email = userData[0].email;
+
+        console.log(this.formData.Fullname);
+
+        this.prefillInput();
+      },
+      methods: {
+        prefillInput() {
+          if (this.formData.Fullname !== "") {
+            $("#nameInputControl").val(this.formData.Fullname);
+          }
+
+          if (this.formData.Email !== "") {
+            $("#emailInputControl").val(this.formData.Email);
+          }
+        },
+        async onSubmit() {
+          this.generalSubmitError = "";
+          console.log('submit')
+          try {
+            const response = await $.ajax({
+              method: "POST",
+              url: "/api/leads",
+              // headers: {
+              //   "g-recaptcha-response": this.recaptchaResponse,
+              // },
+              contentType: "application/json",
+              data: JSON.stringify(this.formData),
+            }).promise();
+            // this.resetFormData();
+            // this.$refs.registerForm.reset();
+            console.log('done')
+          } catch (e) {
+            this.generalSubmitError =
+              "An error has occured while trying to submit the form. Please try again later.";
+          } 
+          // finally {
+          //   this.$refs.reCaptcha.reset();
+          // }
+        },
+      },
+    });
+  }
+});
 
 // $(function() {
-    
+
 //     const contactForm = new Vue({
 //         el: '#contact-form',
 //         data: {
