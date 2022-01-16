7e857d14ecafc156f71643778e1626d88840330e
amrieshahrul <amrieshahrulceo@gmail.com>
2022-01-16 20:15:29 +0800

    feat: prizes layout and styling.

--- src/js/pages/prizes.js
+++ src/js/pages/prizes.js
@@ -1,8 +1,12 @@
 import Swiper from 'swiper';
 
-const swiper = new Swiper('#swiper-collections', {
+const swiper = new Swiper('#swiper-prizes', {
   loop: false,
   slidesPerView: 1,
+  spaceBetween: 30,
+  // width: 340,
+  autoHeight: true,
+  centeredSlides: true,
   breakpoints: {
     992: {
       slidesPerView: 4
@@ -15,15 +19,18 @@ const swiper = new Swiper('#swiper-collections', {
 })
 
 $(function() {
-  const $collectionsSlidesEl = $('#swiper-collections').find('.swiper-slide')
-  const $collectionsSlidesItemEl = $('#swiper-collections').find('.collections-weekly__item-container')
+  const $collectionsSlidesEl = $('#swiper-prizes').find('.swiper-slide')
+  const $collectionsSlidesItemEl = $('#swiper-prizes').find('.prizes-winner-container')
+  const $prizesWrapperContainer = $('#swiper-prizes').find('.prizes-wrapper-container')
 
   function initCollectionSwiper() {
     if ($(window).width() < 768) {
       $collectionsSlidesItemEl.addClass('swiper-slide')
+      // $prizesWrapperContainer.removeClass('row')
       swiper.init()
     } else {
       $collectionsSlidesEl.removeClass('swiper-slide')
+      // $prizesWrapperContainer.addClass('row')
       swiper.destroy()
     }
   }
