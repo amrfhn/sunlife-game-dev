b9cee8ccae4c61d775d57d916f21ec87e1834654
Arash Suhaimi <arash@thebitlab.co>
2022-01-21 20:11:34 +0800

    added item details

---
+++ src/js/components/item-details.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+
+$(function () {
+  if ($("#itemModal").length) {
+    let token = sessionStorage.getItem("game_token");
+    const reward = new Vue({
+      el: "#itemModal",
+      data: {
+        week: {},
+        score: {
+          name: "",
+          image: "",
+          description: "",
+        },
+      },
+      mounted: function () {
+        this.getScoreSubmission();
+        // $(this.$refs.vuemodal).on("show.bs.modal", this.getScoreSubmission);
+      },
+      methods: {
+        getScoreSubmission() {
+          let self = this;
+          $.ajax({
+            type: "GET",
+            url: process.env.API_BASEURL + "/user-score-submission",
+            headers: {
+              Authorization: `Bearer ${token.replaceAll('"', "")}`,
+            },
+            contentType: "application/json",
+            data: JSON.stringify({}),
+          }).done(function (res) {
+            self.score.name = res.data.score[0].name;
+            self.score.image = res.data.score[0].image;
+            self.score.description = res.data.score[0].description;
+          }).fail(function (e){
+            console.log(e)
+          });;
+        },
+      },
+    });
+  }
+});
